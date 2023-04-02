2e81d1da3f2ea90261dd8ee091b1874376f4f429
claudmiine <wojcieska.klaudia@gmail.com>
2023-04-03 00:09:49 +0100

    landing page added, styles added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3 class="joinus"> Join us and let’s change the world together!</h3>
+      <h3>Wasted helps you to make your waste management easier.</h3>
+    <img alt="Recycling photo" class="recycling" src="@/assets/recycling.png" width="300" height="300" />
+          <button to="/">Login</button>
+          <button to="/about">Register</button>
+
+      <h3 class="joinus"> Personal experience</h3>
+      <p>Create personalised calendar for your collection day and more!</p>
+      <br>
+       <img alt="Calendar photo" class="calendar" src="@/assets/calendar.png" width="300" height="250" />
+
+      <h3 class="joinus">Accessible knowledge at your’s fingertipsAccessible knowledge at your’s fingertips</h3>
+      <p>Our information and resources are readily available to you.</p>
+      This project is served and bundled with
+      <br>
+       <img alt="Phone photo" class="phone" src="@/assets/Phone.png" width="300" height="300" />
+  
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+green {
+font-weight: 400;
+}
+.joinus {
+font-weight: bold;
+}
+.greetings h1{
+font-weight: bold;
+color: #333333;
+}
+.greetings h3 {
+  text-align: left;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
